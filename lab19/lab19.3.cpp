65cd408f2e1e84e8806aebb8028f17144fa934d8
alisaocctoi <55703458+alisaocctoi@users.noreply.github.com>
2019-12-26 09:44:18 +0300

    Update lab19.3.cpp

--- lab19/lab19.3.cpp
+++ lab19/lab19.3.cpp
@@ -14,20 +14,20 @@ int main()
 	cin>> n;
 	cout << "введите матрицу" << endl;
 	int** a = new int* [n];
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 		a[i] = new int[m];
-	for (i = 1; i <= n; i++)
-		for (j = 1; j <= m; j++)
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
 			cin >> a[i][j];
 
-	for (i = 1; i <= n; i++) {
-		for (j = 1; j <= m; j++)
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++)
 			cout << a[i][j];
 	}
-	for (j = 1; j <= (m / 2); j++)
+	for (j = 0; j < (m / 2); j++)
 
 	{
-		for (i = 1; i <= (n / 2); i++)
+		for (i =0; i < (n / 2); i++)
 		{
 			c = a[i][j];
 			a[i][j] = a[(n / 2) + i][(m / 2) + j];
@@ -37,8 +37,8 @@ int main()
 
 
 
-	for (i = 1; i <= n; i++) {
-		for (j = 1; j <= m; j++)
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++)
 			cout << a[i][j];
 		cout << endl;
 	}
