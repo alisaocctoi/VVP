12fee105bd0a1f49085f381be56cbddf01b97873
alisaocctoi <55703458+alisaocctoi@users.noreply.github.com>
2019-12-26 09:48:21 +0300

    Update lab19.5!.cpp

--- lab19/lab19.5!.cpp
+++ lab19/lab19.5!.cpp
@@ -1,39 +1,46 @@
 ﻿// lab19.5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+//#include <stdio.h>
 #include <iostream>
 using namespace std;
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int m, i, j, k, c, sum;
-	cout << "Введите m";
+	int	n, m, i, j, c, min, max, k;
+	float sum;
+	cout << "Введите m: ";
 	cin >> m;
-	cout << "введите матрицу";
-	int** a = new int* [m];
-	for (i = 1; i <= m; i++)
-		a[i] = new int[m];
-	for (i = 1; i <= m; i++)
-		for (j = 1; j <= m; j++)
-			cin >> a[i][j];
-
-	for (i = 1; i <= m; i++) {
-		for (j = 1; j <= m; j++)
-			cout << a[i][j];
-	}
 	c = m;
 	sum = 0;
-	for (k = 1; k <= (m * 2 - 1); k++) {
-		for (i = 1; i <= m; i++)
-			for (j = 1; j <= m; j++)
+	int** A = new int* [m];
+	int* A2 = new int[m * 2 - 1];
+	for (i = 0; i < m; i++)
+		A[i] = new int[m];
+	cout << " Введите элементы матрицы: ";
+	for (i = 0; i < m; i++)
+		for (j = 0; j < m; j++)
+			cin >> A[i][j];
+	for (k = 0; k < m * 2 - 1; k++)
+	{
+		for (i = 0; i < m; i++)
+		{
+			for (j = 0; j < m; j++)
+			{
 				if (i == j + c - 1)
-					sum += a[i][j];
-		cout << "сумма  диагонали " << sum<<"\t";
+				{
+					sum = sum + A[i][j];
+				}
+			}
+		}
+		A2[k] = sum;
 		sum = 0;
-		c--;
+		c = c - 1;
 	}
-}
+	cout << "Суммы элементов диагоналей: " << endl;
+	for (k = 0; k < m * 2 - 1; k++) {
+		cout << "Сумма элементов " << k + 1 << "-й диагонали: " << A2[k] << endl;
+	}
 
+}
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
